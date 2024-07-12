bed06f515b0783293e83e52a9951f7a4281464ec
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-07-12 16:08:35 -0400

    Fixing packages/server-core/src/projects/project/downloadProjects.ts for finding 4065

--- packages/server-core/src/projects/project/downloadProjects.ts
+++ packages/server-core/src/projects/project/downloadProjects.ts
@@ -40,7 +40,7 @@ import logger from '../../ServerLogger'
  * @param storageProviderName
  * @returns {Promise<boolean>}
  */
-export const download = async (projectName: string, storageProviderName?: string) => {
+	async (projectName: string, storageProviderName?: string) => {
   if (projectName === 'default-project') return
 
   const storageProvider = getStorageProvider(storageProviderName)
@@ -101,3 +101,60 @@ export const download = async (projectName: string, storageProviderName?: string
     throw e
   }
 }
+
+    let files = await getFileKeysRecursive(`projects/${projectName}/`)
+
+    files = files.filter(
+      (file) =>
+        !file.startsWith(`/projects/${projectName}/assets/`) && !file.startsWith(`/projects/${projectName}/public/`)
+    )
+    logger.info('[ProjectLoader]: Found files:' + files)
+
+    const localProjectDirectory = path.join(appRootPath.path, 'packages/projects/projects', projectName)
+    if (fs.existsSync(localProjectDirectory)) {
+      logger.info('[Project temp debug]: fs exists, deleting')
+      deleteFolderRecursive(localProjectDirectory)
+    }
+
+    await Promise.all(
+      files.map(async (filePath) => {
+        logger.info(`[ProjectLoader]: - downloading "${filePath}"`)
+        const fileResult = await storageProvider.getObject(filePath)
+        if (fileResult.Body.length === 0) {
+          logger.info(`[ProjectLoader]: WARNING file "${filePath}" is empty`)
+          return
+        }
+        writeFileSyncRecursive(path.join(appRootPath.path, 'packages/projects', filePath), fileResult.Body)
+      })
+    )
+
+    logger.info(`[ProjectLoader]: Successfully downloaded and mounted project "${projectName}".`)
+    // if (projectName !== 'default-project') {
+    //   const npmInstallPromise = new Promise<void>((resolve) => {
+    //     const npmInstallProcess = spawn('npm', ['install', '--legacy-peer-deps'], { cwd: localProjectDirectory })
+    //     npmInstallProcess.once('exit', () => {
+    //       logger.info('Finished npm installing %s', projectName)
+    //       resolve()
+    //     })
+    //     npmInstallProcess.once('error', resolve)
+    //     npmInstallProcess.once('disconnect', resolve)
+    //     npmInstallProcess.stdout.on('data', (data) => logger.info(data.toString()))
+    //     npmInstallProcess.stderr.on('data', (data) => logger.error(data.toString()))
+    //   }).then((result) => logger.info(result))
+    //   await Promise.race([
+    //     npmInstallPromise,
+    //     new Promise<void>((resolve) => {
+    //       setTimeout(() => {
+    //         logger.warn(`WARNING: npm installing ${projectName} took too long!`)
+    //         resolve()
+    //       }, 5 * 60 * 1000) // timeout after 5 minutes
+    //     })
+    //   ])
+    // }
+  } catch (e) {
+    const errorMsg = `[ProjectLoader]: Failed to download project ${projectName} with error: ${e.message}`
+    logger.error(e, errorMsg)
+    throw e
+  }
+}
+
